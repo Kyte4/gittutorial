6f4c7957ede8431a1d3d288bbe43b7675dd0c302
kyte4 <zhora_goga_goncharov_2009_gonch@mail.ru>
2023-07-05 14:25:39 +0300

    gg

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import path from 'path'
+import { requestTime,logger } from './middleware.js';
 
 const __dirname = path.resolve()
 const PORT = process.env.PORT ?? 3000
 const app = express()
 
-app.use(express.static(path.resolve(__dirname,'static')))
+app.set('view engine', 'ejs')
+app.set('views',path.resolve(__dirname,'ejs'))
 
+app.use(express.static(path.resolve(__dirname,'static')))
+app.use(requestTime)
+app.use(logger)
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
 //app.get('/', (req,res)=>{
     // res.sendfile(path.resolve(__dirname,'static','index.html'))
     //res.send('<h1>Hello express!<h1>')
@@ -16,9 +25,10 @@ app.use(express.static(path.resolve(__dirname,'static')))
   //  res.sendfile(path.resolve(__dirname,'static','features.html'))
 //})
 
-app.get('/downoload', (req,res)=>{
-    res.download(path.resolve(__dirname,'static','index.html'))
-})
+//app.get('/downoload', (req,res)=>{
+  //  console.log(req.requestTime)
+    //res.download(path.resolve(__dirname,'static','index.html'))
+//})
 
 app.listen(PORT, () => {
     console.log(`server has been started on port ${PORT}`)
