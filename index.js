bf4a177405600011d0d50e3c5fa4b83d75c2e99e
kyte4 <zhora_goga_goncharov_2009_gonch@mail.ru>
2023-07-24 20:14:34 +0300

    gg

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let mess = 5352354
+let mess = 5352354;
 
 function check(chtoto) {
-    if (typeof mess === 'string'){
-      return console.log('mess is string')  
+    if (typeof mess === 'string') {
+        return console.log('mess is string');
     }
 
-    if (typeof mess === 'number'){
-        return console.log('mess is string')
+    if (typeof mess === 'number') {
+        return console.log('mess is string');
     }
 }
-console.log(check(mess))
+console.log(check(mess));
