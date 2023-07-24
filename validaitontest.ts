af6285547a7fd9d9c611c62516918a03916f04a5
kyte4 <zhora_goga_goncharov_2009_gonch@mail.ru>
2023-07-24 20:14:39 +0300

    gg

---
+++ validaitontest.ts
@@ -0,0 +1,8 @@
+function validation(message:string) {
+    const valid = message
+     switch (valid){
+     case valid.match(/\w+@\w+\.\w+/gm) : // mail
+     console.log('eto mail') ;
+    }
+}
+validation('url@mail.ru')
